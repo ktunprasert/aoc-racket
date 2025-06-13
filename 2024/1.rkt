4fc63ecd1a36a8fefcedac9807f6b455d5f32dbc
ktunprasert <ktunprasert@outlook.com>
2025-06-13 18:54:57 +0100

    feat: filename within the file

--- 2024/1.rkt
+++ 2024/1.rkt
@@ -1,7 +1,17 @@
 #lang racket
 
-(time
- (define lists
-   (for/lists (a b #:result (list (sort a <) (sort b <))) ([x (in-port)] [y (in-port)]) (values x y)))
- (displayln (apply foldl (lambda (a b dist) (+ dist (abs (- a b)))) 0 lists))
- (displayln (foldl (λ (x z) (+ (* x (count (curry = x) (second lists))) z)) 0 (first lists))))
+(define args (vector->list (current-command-line-arguments)))
+
+(define filename
+  (if (null? args)
+      "2024/1.txt"
+      (first args)))
+
+(time (define lists
+        (with-input-from-file filename
+                              (lambda ()
+                                (for/lists (a b #:result (list (sort a <) (sort b <)))
+                                           ([x (in-port)] [y (in-port)])
+                                           (values x y)))))
+      (displayln (apply foldl (lambda (a b dist) (+ dist (abs (- a b)))) 0 lists))
+      (displayln (foldl (λ (x z) (+ (* x (count (curry = x) (second lists))) z)) 0 (first lists))))
--- justfile
+++ justfile
@@ -2,7 +2,7 @@ default:
     @just --list
 
 run YEAR DAY *FLAGS:
-    cat {{YEAR}}/{{DAY}}.txt | racket {{FLAGS}} ./{{YEAR}}/{{DAY}}.rkt
+    racket {{FLAGS}} ./{{YEAR}}/{{DAY}}.rkt
 
 example YEAR DAY *FLAGS:
-    cat {{YEAR}}/{{DAY}}e.txt | racket {{FLAGS}} ./{{YEAR}}/{{DAY}}.rkt
+    racket {{FLAGS}} ./{{YEAR}}/{{DAY}}.rkt {{YEAR}}/{{DAY}}e.txt
