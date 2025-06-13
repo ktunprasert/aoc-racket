d18efa3e4c439c60d586bdf192e35cb44e4e7414
ktunprasert <ktunprasert@outlook.com>
2025-06-13 23:45:32 +0100

    2024-3: cooking

---
+++ 2024/3.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+;; (provide input
+;;          part1
+;;          part2)
+
+(require parser-tools/lex)
+(require threading)
+
+(define args (vector->list (current-command-line-arguments)))
+
+(define has-flag? (lambda (flag) (member flag args)))
+
+(define filename
+  (let ([non-flags (filter (lambda (arg) (not (string-prefix? arg "--"))) args)])
+    (if (null? non-flags)
+        "2024/3.txt"
+        (first non-flags))))
+
+;; (define sample-input "xmul(69,420)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+;; (define (part1-get-tokens a-lexer input-string)
+;;   (define p (open-input-string input-string))
+;;   (sequence->list (in-producer (lambda () (a-lexer p)) eof-object?)))
+
+(define part1-lexer
+  (lexer [(eof) eof]
+         ["mul" 'mul]
+         ["(" 'left-paren]
+         [")" 'right-paren]
+         ["," 'comma]
+         [(repetition 1 +inf.0 numeric) (string->number lexeme)]
+         ; Skip everything else - any single character that doesn't match above
+         [any-char (part1-lexer input-port)]))
+
+(define input
+  (with-input-from-file filename
+                        (lambda ()
+                          (sequence->list (in-producer (lambda () (part1-lexer (current-input-port)))
+                                                       eof-object?)))))
+
+(define (part1 input)
+  (~> (filter number? input)
+      (in-slice 2 _)
+      sequence->list
+      (foldl (λ (pair acc) (displayln pair) (+ acc (apply * pair))) 0 _)
+      )
+  )
+
+(define (part2 input)
+  "TODO: Implement part 2")
+
+(when (has-flag? "--output")
+  (printf "Input: ~a~n" input))
+
+(if (has-flag? "--time")
+    (begin
+      (printf "Part 1: ")
+      (time (displayln (part1 input)))
+      (printf "Part 2: ")
+      (time (displayln (part2 input))))
+    (begin
+      (displayln (part1 input))
+      (displayln (part2 input))))
