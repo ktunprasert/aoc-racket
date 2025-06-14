eac8bf20ec6def36cdc00323ff3356f9f3dd9c75
ktunprasert <ktunprasert@outlook.com>
2025-06-14 01:53:00 +0100

    2024-4: wip

--- 2024/4.rkt
+++ 2024/4.rkt
@@ -1,8 +1,10 @@
 #lang racket
 
-(provide input
+(provide :input
          part1
          part2)
+
+(require threading)
 
 (define args (vector->list (current-command-line-arguments)))
 
@@ -14,28 +16,63 @@
         "2024/4.txt"
         (first non-flags))))
 
-(define input
+(define :input
   (time (display "Parsing took: ")
         (with-input-from-file filename
                               (lambda ()
                                 (for/vector ([line (in-lines)])
-                                  line)))))
+                                  (~> line string->list list->vector))))))
+
+;NW N NE
+;W _ E
+;SE S SW
+(define deltas '((-1 -1) (-1 0) (-1 1) (0 -1) (0 1) (1 -1) (1 0) (1 1)))
+(define token (string->list "XMAS"))
+
+(define (grid-at x y)
+  (~> :input (vector-ref _ x) (vector-ref _ y)))
+
+; pos (0 0)
+; delta (0 0)
+(define (word-search x y dx dy)
+  (let* ([x (+ x dx)]
+         [y (+ y dy)]
+         [seq-x (if (zero? dx)
+                    (in-cycle (in-list (list x))) ; cycle just x
+                    (in-range x (+ x (* 3 dx)) dx))] ; proper range
+         [seq-y (if (zero? dy)
+                    (in-cycle (in-list (list y))) ; cycle just y
+                    (in-range y (+ y (* 3 dy)) dy))]) ; proper range
+
+    (for/fold ([ok? #t])
+              ([should "MAS"]
+               [cx seq-x]
+               [cy seq-y])
+      ;; #:final (not ok?)
+      #:break (not ok?)
+      (displayln (list "coords:" cx cy "expected:" should "ok?" ok?))
+      (displayln (list "grid-at:" (grid-at cx cy)))
+      (char=? (grid-at cx cy) should))))
 
 (define (part1 input)
-  "TODO: Implement part 1")
+  ;; (displayln (grid-at 0 5))
+  (displayln (word-search 0 5 0 -1))
+
+  "TODO: Implement part 1"
+  )
 
 (define (part2 input)
   "TODO: Implement part 2")
 
 (when (has-flag? "--output")
-  (printf "Input: ~a~n" input))
+  (printf "Input: ~a~n" :input))
 
 (if (has-flag? "--time")
     (begin
       (printf "Part 1: ")
-      (time (displayln (part1 input)))
+      (time (displayln (part1 :input)))
       (printf "Part 2: ")
-      (time (displayln (part2 input))))
+      (time (displayln (part2 :input))))
     (begin
-      (displayln (part1 input))
-      (displayln (part2 input))))
+      (displayln (part1 :input))
+      (displayln (part2 :input))))
